037a6310838b8b9fd4d56cb2ea10910696a48fbd
Umesh Tiwari <klau.tiwari1234@gmail.com>
2025-05-25 21:39:17 +0530

    User service updated

--- examui/src/app/services/user.service.ts
+++ examui/src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import baseUrl from './helper';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,4 +16,27 @@ export class UserService {
   public addUser(user: any) {
     return this.http.post(`${baseUrl}/user/`, user);
   }
+
+  // get user
+  public getSingleUser(username: any) {
+    return this.http.get(`${baseUrl}/user/${username}`)
+  }
+
+  // update user
+  public updateUser(user: any) {
+    return this.http.put(`${baseUrl}/user/${user.id}`, user);
+  }
+
+  // verify password
+  public verifyPassword(oldPassword: any, user: any): Observable<boolean> {
+    return this.http.put(`${baseUrl}/verify-password/${oldPassword}`, user).pipe(
+      map(response => response as boolean)
+    );
+  }
+
+  // update password
+  public updatePassword(newPassword: any, user: any) {
+    return this.http.put(`${baseUrl}/update-password/${newPassword}`, user);
+  }
+
 }
